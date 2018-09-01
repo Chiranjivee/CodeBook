ce3c19918826722985e8b75380016b5e4ed59651
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-02 02:49:37 +0530

    Add registerServiceWorker in index file

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Pages from './pages';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Pages />, document.getElementById('root'));
+registerServiceWorker();

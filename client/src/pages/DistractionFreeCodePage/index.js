2552ab4298a073e9dbcaa359c62f335ccc17982d
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-02 23:28:57 +0530

    Add github pages support

---
+++ client/src/pages/DistractionFreeCodePage/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class DistractionFreeCodePage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <textarea
+          className="fullscreen"
+          placeholder="Type your code here."
+        />
+      </div>
+    );
+  }
+}
+
+export default DistractionFreeCodePage;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from './HomePage';
 import CodePage from './CodePage';
+import DistractionFreeCodePage from './DistractionFreeCodePage';
 
 class Pages extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <div>
-          <Route exact path="/distraction-free-code-editor/client/build/" component={HomePage} />
-          <Route exact path="/distraction-free-code-editor/client/build/:id" component={CodePage} />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/:id/code" component={CodePage} />
+          <Route path="/code" component={DistractionFreeCodePage} />
         </div>
       </Router>
     )
